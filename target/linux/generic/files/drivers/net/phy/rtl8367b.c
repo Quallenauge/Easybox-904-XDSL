060e8b38ee56313664777d25cc72e0d868c67961
Vitaly Chekryzhev <13hakta@gmail.com>
2017-02-10 12:37:53 +0500

    generic: rtl8366_smi/rtl8367b: update chip detection
    Extend rtl8366_smi with chip_ver field for storing chip ID.
    Useful for drivers supporting multiple chip versions.
    Replace method calls with macro.
    Receive chip number and mode only if detection error occured.
    
    Signed-off-by: Vitaly Chekryzhev <13hakta@gmail.com>

--- target/linux/generic/files/drivers/net/phy/rtl8367b.c
+++ target/linux/generic/files/drivers/net/phy/rtl8367b.c
@@ -1454,41 +1454,28 @@ static int rtl8367b_detect(struct rtl8366_smi *smi)
 	u32 chip_num;
 	u32 chip_ver;
 	u32 chip_mode;
-	int ret;
+	int err;
 
-	/* TODO: improve chip detection */
-	rtl8366_smi_write_reg(smi, RTL8367B_RTL_MAGIC_ID_REG,
-			      RTL8367B_RTL_MAGIC_ID_VAL);
+	REG_WR(smi, RTL8367B_RTL_MAGIC_ID_REG, RTL8367B_RTL_MAGIC_ID_VAL);
 
-	ret = rtl8366_smi_read_reg(smi, RTL8367B_CHIP_NUMBER_REG, &chip_num);
-	if (ret) {
-		dev_err(smi->parent, "unable to read %s register\n",
-			"chip number");
-		return ret;
-	}
-
-	ret = rtl8366_smi_read_reg(smi, RTL8367B_CHIP_VER_REG, &chip_ver);
-	if (ret) {
+	err = rtl8366_smi_read_reg(smi, RTL8367B_CHIP_VER_REG, &chip_ver);
+	if (err) {
 		dev_err(smi->parent, "unable to read %s register\n",
 			"chip version");
-		return ret;
-	}
-
-	ret = rtl8366_smi_read_reg(smi, RTL8367B_CHIP_MODE_REG, &chip_mode);
-	if (ret) {
-		dev_err(smi->parent, "unable to read %s register\n",
-			"chip mode");
-		return ret;
+		return err;
 	}
 
 	switch (chip_ver) {
-	case 0x1000:
-		chip_name = "8367RB";
-		break;
-	case 0x1010:
-		chip_name = "8367R-VB";
-		break;
+		case 0x1000:
+			chip_name = "8367RB";
+			break;
+		case 0x1010:
+			chip_name = "8367R-VB";
+			break;
 	default:
+		REG_RD(smi, RTL8367B_CHIP_NUMBER_REG, &chip_num);
+		REG_RD(smi, RTL8367B_CHIP_MODE_REG, &chip_mode);
+
 		dev_err(smi->parent,
 			"unknown chip num:%04x ver:%04x, mode:%04x\n",
 			chip_num, chip_ver, chip_mode);
@@ -1496,6 +1483,8 @@ static int rtl8367b_detect(struct rtl8366_smi *smi)
 	}
 
 	dev_info(smi->parent, "RTL%s chip found\n", chip_name);
+
+	smi->chip_ver = chip_ver;
 
 	return 0;
 }
