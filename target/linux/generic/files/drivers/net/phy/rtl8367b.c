71115fd1b813d0de58ce979d87534b5e14c4be4a
Vitaly Chekryzhev <13hakta@gmail.com>
2017-02-10 09:03:04 +0500

    generic: rtl8367b: fix and improve initvals write
    Previously rlvid was wrongly converted and for different chip (0x1000, 0x1010)
    it was equal 1 what caused loading improper values to RTL8367RB switch.
    Load initvals depending on raw chip ID.
    
    Signed-off-by: Vitaly Chekryzhev <13hakta@gmail.com>

--- target/linux/generic/files/drivers/net/phy/rtl8367b.c
+++ target/linux/generic/files/drivers/net/phy/rtl8367b.c
@@ -688,31 +688,22 @@ static int rtl8367b_write_phy_reg(struct rtl8366_smi *smi,
 static int rtl8367b_init_regs(struct rtl8366_smi *smi)
 {
 	const struct rtl8367b_initval *initvals;
-	u32 chip_ver;
-	u32 rlvid;
 	int count;
-	int err;
 
-	REG_WR(smi, RTL8367B_RTL_MAGIC_ID_REG, RTL8367B_RTL_MAGIC_ID_VAL);
-	REG_RD(smi, RTL8367B_CHIP_VER_REG, &chip_ver);
+	switch (smi->chip_ver) {
+		case 0x1000:
+			initvals = rtl8367r_vb_initvals_0;
+			count = ARRAY_SIZE(rtl8367r_vb_initvals_0);
+			break;
 
-	rlvid = (chip_ver >> RTL8367B_CHIP_VER_RLVID_SHIFT) &
-		RTL8367B_CHIP_VER_RLVID_MASK;
+		case 0x1010:
+			initvals = rtl8367r_vb_initvals_1;
+			count = ARRAY_SIZE(rtl8367r_vb_initvals_1);
+			break;
 
-	switch (rlvid) {
-	case 0:
-		initvals = rtl8367r_vb_initvals_0;
-		count = ARRAY_SIZE(rtl8367r_vb_initvals_0);
-		break;
-
-	case 1:
-		initvals = rtl8367r_vb_initvals_1;
-		count = ARRAY_SIZE(rtl8367r_vb_initvals_1);
-		break;
-
-	default:
-		dev_err(smi->parent, "unknow rlvid %u\n", rlvid);
-		return -ENODEV;
+		default:
+			dev_err(smi->parent, "unknown chip %u\n", smi->chip_ver);
+			return -ENODEV;
 	}
 
 	/* TODO: disable RLTP */
