c1a9f81969283905d17615dae3b8c3210bcee6b8
Quallenauge <Hamsi2k@freenet.de>
2020-01-16 23:24:51 +0800

    VGV952CJW33-E-IR: Revert subpage handling in upgrade scripts.
    The kernel handles these by itself since some time ago.
    
    Revert "Use partition 'ubi' (mtd12) for rootfs and rootfs_data; use 512 UBI sub-blocks"
    
    This reverts commit fabfbe6bf19696f2fc211586ba4875b14c7bd58e.
    
    Revert "VGV952CJW33-E-IR: Sysupgrade: Deactivate subpages on nand."
    
    This reverts commit ac64e9e820be47dffa5b967d613fca2cfc876a1c.

--- target/linux/lantiq/xrx200/base-files/lib/upgrade/platform.sh
+++ target/linux/lantiq/xrx200/base-files/lib/upgrade/platform.sh
@@ -15,26 +15,11 @@ platform_do_upgrade() {
 	avm,fritz7412|\
 	bt,homehub-v5a|\
 	zyxel,p-2812hnu-f1|\
-	zyxel,p-2812hnu-f3|\
-	lantiq,vgv952cjw33-e-ir)
+	zyxel,p-2812hnu-f3)
 		nand_do_upgrade $1
 		;;
 	*)
 		default_do_upgrade "$1"
-		;;
-	esac
-}
-
-platform_nand_pre_upgrade() {
-	local board=$(board_name)
-	echo "platform_nand_pre_upgrade()"
-
-	case "$board" in
-	lantiq,vgv952cjw33-e-ir )
-		# This is a global defined in nand.sh, sets another VID header offset than default.
-		# (now disabled, as it looks like eb904 can handle 512 byte subpages as reported by driver)
-		#echo "Set header offset"
-		#CU_UBI_VID_HEADER_OFFSET="2048"
 		;;
 	esac
 }
